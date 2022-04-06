0c0058c8dc55c2682da07285703b38f0ce4d4bfc
Hipjea <pierre.duverneix@gmail.com>
2022-04-06 16:14:26 +0200

    setup script

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 docker-compose up -d
 docker-compose exec wordpress composer install --working-dir=/var/www/html/wp-content/themes/unt
 ```
+
+### Use NPM
+
+```
+docker-compose exec wordpress npm run watch --prefix wp-content/themes/unt
+```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 WORDPRESS_SRC=$PWD/wordpress/src
+WORDPRESS_PLUGINS=$PWD/wordpress/src/wp-content/plugins
 WORDPRESS_URL=https://fr.wordpress.org/latest-fr_FR.tar.gz
 THEME_GIT=https://github.com/Hipjea/unt
 
@@ -29,9 +30,36 @@ if [ -d "ls -A $dir" ]; then
     fi
 fi
 
+# install theme and plugins
 if [ ! -d "ls -A $PWD/wordpress/src" ]; then
     cd $WORDPRESS_SRC
     cp ../unt-config.php ./
-    cd $WORDPRESS_SRC/wp-content/themes
-    git clone $THEME_GIT
+
+    if [ -d "ls -A $WORDPRESS_SRC/wp-content/themes/unt" ]; then
+        cd $WORDPRESS_SRC/wp-content/themes
+        git clone $THEME_GIT
+    fi
+    echo $($WORDPRESS_PLUGINS/acf)
+    if [ ! -f "$WORDPRESS_PLUGINS/acf" ]; then
+        cd $WORDPRESS_PLUGINS
+        git clone https://github.com/AdvancedCustomFields/acf
+    fi
+fi
+
+# add some default configuration to the database
+cd $PWD/../../../../
+if [ -f ./.env ]; then
+    docker-compose up -d
+    CONTAINERS=$(docker ps --format '{{.Names}}:{{.Image}}' | grep 'mysql\|mariadb' | cut -d":" -f1)
+    CONTAINER=${CONTAINERS[0]}
+    source ./.env
+    docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_ROOT_PASSWORD \
+        ${CONTAINER[0]} mysql -u root $MYSQL_DATABASE -e \
+        "UPDATE wp_options SET option_value = '/%year%/%monthnum%/%day%/%postname%/' WHERE option_name = 'permalink_structure'"
+    docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_ROOT_PASSWORD \
+        ${CONTAINER[0]} mysql -u root $MYSQL_DATABASE -e \
+        "UPDATE wp_options SET option_value = 'a:1:{i:0;s:11:\"acf/acf.php\";}' WHERE option_name = 'active_plugins'"
+    docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_ROOT_PASSWORD \
+        ${CONTAINER[0]} mysql -u root $MYSQL_DATABASE -e \
+        "SELECT option_name, option_value FROM wp_options WHERE option_name = 'active_plugins'"
 fi
